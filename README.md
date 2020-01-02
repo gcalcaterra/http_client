eafe717d740b428360ba70d56ed55ad4ff97f2e8
unknown <gustavocalcaterra@gmail.com>
2020-01-02 11:57:12 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# http_client
+ # test-get-https
+test_get_https es una aplicación que permite realizar test de los certificados agregando la dirección https y pasando el trust que deseamos probar.
+
+
+
+## Requisitos
+
+* JDK 1.8
+* Maven
+
+## Instalación
+
+```bash
+$ git clone PROJECT_URL
+$ cd test_get_https
+$ mvn clean install package 
+```
+
+
+
+## Ejemplo
+
+```bash
+$ java -jar -Djavax.net.debug=all -Djavax.net.ssl.trustStore=/opt/example.keystore -Djavax.net.ssl.trustStorePassword=user@password test_get_https-0.0.1-SNAPSHOT-jar-with-dependencies.jar https://www.example.com
+```
+
+
+
---
+++ src/main/java/py/com/data/test_get_https/App.java
@@ -0,0 +1,63 @@
+package py.com.data.test_get_https;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        
+    
+
+        try {
+
+            HttpGet request = new HttpGet(args[0]);
+
+            // add request headers
+            request.addHeader("custom-key", "mkyong");
+            request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            try {
+
+                // Get HttpResponse Status
+                System.out.println(response.getProtocolVersion());              // HTTP/1.1
+                System.out.println(response.getStatusLine().getStatusCode());   // 200
+                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return it as a String
+                    String result = EntityUtils.toString(entity);
+                    System.out.println(result);
+                }
+
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+
+    }
+	
+	
+	
+	
+}
